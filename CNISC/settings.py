93cacbe25511f202ec61602402ece18f65359273
Jonathan Alvarez González <jonas.al.co@gmail.com>
2014-09-25 22:48:21 -0500

    Organizando sentencias estáticas del settings

--- CNISC/settings.py
+++ CNISC/settings.py
@@ -90,11 +90,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (    os.path.join (BASE_DIR,'public/static') ,)
+STATICFILES_DIRS = ( os.path.join (BASE_DIR,'public/static'), )
+MEDIA_ROOT = ( os.path.join (BASE_DIR,'public/media') )
 
 STATIC_URL= '/static/'
-MEDIA_ROOT = (    os.path.join (BASE_DIR,'public/media') )
 MEDIA_URL = '/media/'
 
