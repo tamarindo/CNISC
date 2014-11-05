70e07574d0dfecde3165f96f2ea0ad0b1781d083
Jonathan Alvarez <j@jonalvarezz.com>
2014-11-04 22:20:36 -0500

    Cargando lista mensajes privados

--- public/static/js/app/messaging/messaging.main.js
+++ public/static/js/app/messaging/messaging.main.js
@@ -10,22 +10,29 @@
 	messaging.controller('messages', ['$scope', function($scope){
 
 		// Initiate
-		var lastActiveIndex = 0;
-		$scope.list = window.vector_messages['mensajes'];
-		$scope.activeMessage = $scope.list[0];
+		var lastActiveIndex = [0, 0]; // last active index, last active list
+		$scope.list = [
+			vector_messages['mensajes-privados'],
+			vector_messages['mensajes']
+		];
+
+		$scope.activeMessage = $scope.list[0][0]; // First message of mensajes array
 		$scope.activeMessage.isActive = true;
 
 		// Show the selected message
-		$scope.show = function( index ) {
-			if( lastActiveIndex != index ) {
+		$scope.show = function( index, list ) {
+			var lastIndex = lastActiveIndex[0];
+			var lastList = lastActiveIndex[1];
+			
+			if( lastIndex != index || lastList != list ) {
 
 				// Update the last active item
-				$scope.list[lastActiveIndex].isActive = false;
-				lastActiveIndex = index;
+				$scope.list[lastList][lastIndex].isActive = false;
+				lastActiveIndex = [index, list];
 
 				// Update new active item
-				$scope.list[index].isActive = true;
-				$scope.activeMessage = $scope.list[index];
+				$scope.list[list][index].isActive = true;
+				$scope.activeMessage = $scope.list[list][index];
 			}
 		}
 
