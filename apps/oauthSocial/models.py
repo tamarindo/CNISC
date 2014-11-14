313e4c2d77ff5544301135010026e3285c40f6a8
Daniel Bernal <tamarindodn@hotmail.com>
2014-11-13 20:24:10 -0500

    funciones de vincular cuenta twitter

--- apps/oauthSocial/models.py
+++ apps/oauthSocial/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from apps.main.models import *
 
-# Create your models here.
+from apps.userManager.models import Profile, baseModel
+
+Provedores = (
+    ('Facebook', 'Facebook'),
+    ('Twitter', 'Twitter'),
+)
+
+# Messages
+class App(baseModel):
+	consumer_key = models.CharField(max_length=400, verbose_name=_("consumer_key"), null=True, blank=True)
+	consumer_secret= models.CharField(max_length=400, verbose_name=_("consumer_secret"), null=True, blank=True)
+	nombre = models.CharField(max_length=30, verbose_name=_("nombre"))
+	provedor =  models.CharField(max_length=1, choices=Provedores, verbose_name=_("Provedores"))
+	callback_url = models.CharField(max_length=500, verbose_name=_("callback url"))
+	
+	def __unicode__(self):
+		return self.nombre
+
+class CuentaSocial(baseModel):
+	user = models.ForeignKey(User, verbose_name=_("usuario"))
+	uid =  models.CharField(max_length=400, verbose_name=_("uid"), null=True, blank=True)
+	app = models.ForeignKey(App, verbose_name=_("App"))
+	select = models.BooleanField(default=False)
+	def __unicode__(self):
+		return self.user.username
+
+
+class TokenSocial(baseModel):
+
+	cuenta =  models.ForeignKey(CuentaSocial, verbose_name=_("CuentaSocial"))
+	token = models.CharField(max_length=500, verbose_name=_("token"))
+	token_secreto = models.CharField(max_length=500, verbose_name=_("token secreto"))
+	fecha_expiracion =  models.DateTimeField(verbose_name=_("fecha expiracion"))
+	
+	
+	def __unicode__(self):
+		return self.cuenta.nombre+" de "+" :"+self.cuenta.user
+
--- apps/oauthSocial/views.py
+++ apps/oauthSocial/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login , logout
 
-# Create your views here.
+
+from twython import Twython
+
+
+def autentificar_usuario_twitter(request):
+
+	ob_app=App.object.get(provedor="Twitter")
+	APP_KEY = ob_app.consumer_key
+	APP_SECRET = ob_app.consumer_secret
+
+	ob_cuenta_twitter=CuentaSocial.object.get_or_none(user=request.user)
+	twitter = Twython(APP_KEY, APP_SECRET)
+	auth = twitter.get_authentication_tokens(callback_url='localhost:8000/callback')	
+	OAUTH_TOKEN = auth['oauth_token']
+	OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+
+
+
+	if !ob_cuenta_twitter:
+		new_cuenta_twitter = CuentaSocial(user=request.user,select=1,app=ob_app)
+		new_cuenta_twitter.save()
+		token_social = TokenSocial(cuenta=new_cuenta_twitter,token=OAUTH_TOKEN,token_secreto=OAUTH_TOKEN_SECRET)
+		token_social.save()
+	
+	else :
+		ob_token = TokenSocial.object.get_or_none(cuenta=ob_cuenta_twitter)
+
+		if !ob_token :
+		ob_token.token(OAUTH_TOKEN)
+		ob_token.token_secreto(OAUTH_TOKEN_SECRET)
+		ob_token.save()
+
+		else:  
+		token_social = TokenSocial(cuenta=cuenta_twitter,token=OAUTH_TOKEN,token_secreto=OAUTH_TOKEN_SECRET)
+		token_social.save()
+
+    return HttpResponseRedirect(auth['auth_url'])
+
+
+def callbacktwitter(request):
+
+	ob_app=App.object.get(provedor="Twitter")
+	ob_cuenta_twitter=CuentaSocial.object.get_or_none(user=request.user)
+	ob_token = TokenSocial.object.get_or_none(cuenta=ob_cuenta_twitter)	
+	
+	if  ob_app and ob_cuenta_twitter and ob_token :
+
+		oauth_verifier=request.GET.get('oauth_verifier')
+		twitter = Twython(ob_app.consumer_key, ob_app.consumer_secret,ob_token.token, ob_token.token_secreto)
+		final_step = twitter.get_authorized_tokens(oauth_verifier)
+		ob_token.token = final_step['oauth_token']
+		ob_token.token_secreto =  final_step['oauth_token_secret']
+	
+	return HttpResponseRedirect(reverse("preferences"))	
+
