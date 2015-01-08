905c705396fbe3c84e9b3d6e4718765afd1fcbb8
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-01-07 21:43:51 -0500

    Los mensajes ahora pueden mostrar un resumen corto

--- public/static/js/app/user/user.main.js
+++ public/static/js/app/user/user.main.js
@@ -21,7 +21,7 @@
 			$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 		}]);
 
-	app.controller('mainController', [ '$scope', 'ApiMarkAllMessagesAsSeen' ,function($scope, ApiMarkAllMessagesAsSeen){
+	app.controller('mainController', [ '$scope', '$timeout', 'ApiMarkAllMessagesAsSeen' ,function($scope, $timeout, ApiMarkAllMessagesAsSeen){
 		
 		$scope.list = [
 			vector_messages['mensajes-privados'],
@@ -45,6 +45,27 @@
 			// Ocultar menú lateral
 			$scope.showMenu = false;
 
+		};
+
+		// Muestra/esconde un pequeño resumen de cada mensaje del sidebar
+		// @param bool
+		$scope.toggleMessageExcerpt = function( show ) {
+			$scope.showMessageExcerpt = show;
+
+			// Grabar esta preferencia en la BD
+			// Nos gusta jugar con estas opciones, así que envie la solicitud
+			// luego de transcurridos 3.5 segundos.
+			// Son 3.5s porque YOLO
+
+			// Si hay algun request efectúandose, cancélese y 
+			// genere uno nuevo.
+			if( $timeout.cancel($scope.toggleMessageExcerptRequest) ) {
+				console.log('Cancelando solicitud');
+			}
+			console.log('Enviando solicitud');
+			$scope.toggleMessageExcerptRequest = $timeout(function(){
+				console.log('Solicitud enviada: ' + $scope.showMessageExcerpt);
+			}, 3500);
 		};
 	
 	}]);
