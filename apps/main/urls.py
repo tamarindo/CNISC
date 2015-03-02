54e8fb742652492e88d131da078194cbdf7ce36f
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-03-02 00:03:24 -0500

    Agregando URL de crear usuarios sin Trailing SLASH para maximizar compatibilidad con request POST de Angular

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -13,6 +13,7 @@ main_urls = patterns('apps.main.views',
 
 	url(r'^usuarios$','panelUseradmin',name="panelUseradmin"),
     url(r'^usuario/editar/(\d+)', Usuario.as_view() ,name="usereditaradmin"),
+    url(r'^usuario/crear$', 'panelCrearUsuarios' ,name="panelCrearUsuarios"),
     url(r'^usuario/crear/$', 'panelCrearUsuarios' ,name="panelCrearUsuarios"),
 
 )
