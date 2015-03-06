89dafde06aa0c01bc62f36aff69b0b3869883b5a
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-03-06 18:56:56 -0500

    ( #10 ) Ahora el módulo de buscar usuarios es un módulo independiente.

--- public/static/js/app/admin/admin.searchUsers.js
+++ public/static/js/app/admin/admin.searchUsers.js
@@ -1,8 +1,13 @@
-(function() {
+/*
+ * Módulo sobre la búsqueda de usuarios para el admin
+ * @url /usuarios
+*/
+
+(function(){
+
 	'use strict';
 
-	var app = angular.module('admin', 
-		['ngCookies', 'admin-controllers'])
+	var app = angular.module('searchUsers', ['ngCookies', 'Api'])
 
 		// Cambiar el control de expresiones para prevenir 
 		// inconvenientes con las de Django.
@@ -17,5 +22,14 @@
 			$http.defaults.headers.put['X-CSRFToken'] = $cookies.csrftoken;
 			$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 		}]);
+
+
+	// Controlador que recoge los usuarios de una variable enviada 
+	// renderizada por Django para iterar sobre ella y listar los usuarios
+	app.controller('SearchController', ['$scope', function($scope){
+
+		$scope.users = window.sresu;
+
+	}]);
 
 })();
