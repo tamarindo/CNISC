25d9f13fea53f4c7b3cabcff1b21783e02c2b405
Daniel Bernal <tamarindodn@hotmail.com>
2015-03-11 20:51:41 -0500

    recuperar contraseña

--- apps/userManager/views.py
+++ apps/userManager/views.py
@@ -52,7 +52,6 @@ def recuperar_pass(request):
 		EmailV = re.match("^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,10}$",email)
 		if EmailV != None:
 			ob_user = User.objects.filter(email = email)
-			pprint.pprint(ob_user)
 	 		if not ob_user:
 				ob_user = UserExt.objects.filter(email_alt = email)				
 
@@ -68,38 +67,61 @@ def recuperar_pass(request):
 					'key'    : p,
 			        'titulo' : 'Recupera Password',
 			        'url'    : 'localhost:8000/verificarKeys', 
-			        'usuario': ob_user.get_full_name(),
+			        'usuario': ob_user[0].get_full_name(),
 			    }
 				email_html = render_to_string('email_keys_send.html', email_context)
 
 			    # se quitan las etiquetas html para que quede en texto plano
-				email_text = strip_tags(email_html)
+				# email_text = strip_tags(email_html)
 			 	
 				correo = EmailMultiAlternatives(
 			        'Recupera Password CNISC',  # Asunto
-			        email_text,  # contenido del correo
-			        'danimuerto@gmail.com',  # quien lo envía
+			        email_html,  # contenido del correo
+			        'no-reply@isc.edu.co',  # quien lo envía
 			        [email],  # a quien se envía
 			    )
 			 
 			    # se especifica que el contenido es html
-				correo.attach_alternatives(email_html, 'text/html')
+				correo.attach_alternative(email_html, 'text/html')
 			    # se envía el correo
 				correo.send()
 
+				mensaje = 'Se ha enviado un correo con las istruciones para recuperar su contraseña'
+
+			else :
+				mensaje = 'No existe este algun usuario con este correo'
+		else :
+			mensaje = 'El correo no tiene el formato correcto'
+	else :
+		mensaje = ''
 	template = 'recuperar_pass.html'
-	return render_to_response(template,{},context_instance=RequestContext(request))
+	return render_to_response(template,{'mensaje':mensaje},context_instance=RequestContext(request))
 
 
-def verificar_keys(request):
+def verificar_keys(request,key):
 	if request.method == 'POST':
-		fromrp = from_recuperar_pass
 
-		
-	else:
-		pass
-	fromrp = from_recuperar_pass
-	return render_to_response(template,{'formulario':formularioLogin},context_instance=RequestContext(request))
+		ob_key = TempKeys.objects.get_or_none(key=key)
+		if ob_key :
+			hora_actual = datetime.now()	
+			delta = timedelta(hours=-24)
+			if key.date_added + delta <= hora_actual :
+				email=request.POST.get('password1')
+				email=request.POST.get('password2')
+				if password1 == password2:
+					ob_user = User.objects.get(pk=key.user.pk)
+					ob_user.set_password("password1")
+					ob_user.save()
+					return HttpResponseRedirect(reverse("login", kwargs={'msj': 'contraseña actulizada'}))
+				else :
+					mensaje = " Las password no coinciden "
+			else :
+				mensaje = "La Key a vencido porfavor genera una nueva"
+		else :
+			mensaje = " key no valida"		
+	
+	template = 'verificar_pass.html'
+	return render_to_response(template,{},context_instance=RequestContext(request))
 
 # -------------------------------------------- API V2 ---------------------------------------------
 		
