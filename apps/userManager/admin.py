1725b266c17e7e667b32273baf49b048614cee14
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-04-08 19:00:08 -0500

    Eliminando campos innecesarios tras PR #17

--- apps/userManager/admin.py
+++ apps/userManager/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.models import User
 # Register your models here.
+
 admin.site.register(UserExt)
 admin.site.register(Profile)
-admin.site.register(Graduate)
-admin.site.register(Student)
+admin.site.register(ProfileMeta)
 admin.site.register(TempKeys)
