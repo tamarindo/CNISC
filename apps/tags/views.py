41a127e514c6cbb32da585165b39a995458a0dbd
Daniel Bernal <tamarindodn@hotmail.com>
2015-04-13 20:24:46 -0500

    avance tags backend

--- apps/tags/views.py
+++ apps/tags/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-
+from apps.userManager.models import  Profile 
 import pprint
 import json
 # Create your views here.
@@ -12,31 +12,25 @@ def autocomplete(request,*args):
 	pprint.pprint(args)
 	if request.method == 'GET' :
 		table = 'usuarios'
-		key = args
+		key = args[0]
 
 		vec_user=[]
-		vec_user_user=[]
-		vec_user_enge=[]
-		est = [] 
-		eng = []
+		vec_user_meta_filter=[]
 
 		if table == 'usuarios' :
-			if key == "estudiate":
-				est = User.objects.filter(userext__profile__name=key)
+			profile=Profile.objects.get_or_none(name=key)
+			if key == "estudiate" or key == 'engresado':
+
+				est = User.objects.filter(userext__profile=profile)
 				for ob_user in est:
-					vec_user_user.appent(dict([('username',ob_user.username),('fist_name',ob_user.fist_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
+					vec_user_meta_filter.append(dict([('username',ob_user.username),('first_name',ob_user.first_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
 
-			elif key == 'engresado':
-				eng = User.objects.filter(userext__profile__name=key)
-				for ob_user in eng:
-					vec_user_enge.appent(dict([('username',ob_user.username),('fist_name',ob_user.fist_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
-
-			ob_users= User.objects.filter(username=key,fist_name__contains=key, last_name__contains=key,email__contains=key)
+			ob_users= User.objects.filter(username=key,first_name__contains=key, last_name__contains=key,email__contains=key)
 			
 			for ob_user in ob_users:
-				vec_useSr.appent(dict([('username',ob_user.username),('fist_name',ob_user.fist_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
-			vec_total= vec_user_enge | vec_user_enge | vec_user
-			retorno = {'error':0}
+				vec_user.append(dict([('username',ob_user.username),('first_name',ob_user.first_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
+			vec_total= list(set(vec_user_meta_filter) | set(vec_user))
+			retorno = {'error':0,'data':vec_total}
 		else:
 			retorno = {'error':1}						
 	else:
