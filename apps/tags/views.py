1ddadd50abe4fa0418a5e0ad987fdbd561b7724d
Daniel Bernal <tamarindodn@hotmail.com>
2015-04-14 01:43:06 -0500

    api de tags funcional

--- apps/tags/views.py
+++ apps/tags/views.py
@@ -9,28 +9,28 @@ import json
 
 
 def autocomplete(request,*args):
-	pprint.pprint(args)
 	if request.method == 'GET' :
 		table = 'usuarios'
 		key = args[0]
 
 		vec_user=[]
-		vec_user_meta_filter=[]
-
+		ob_users= User.objects.none()
+		tags_user= User.objects.none()
+		
 		if table == 'usuarios' :
 			profile=Profile.objects.get_or_none(name=key)
-			if key == "estudiate" or key == 'engresado':
-
-				est = User.objects.filter(userext__profile=profile)
-				for ob_user in est:
-					vec_user_meta_filter.append(dict([('username',ob_user.username),('first_name',ob_user.first_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
-
+			pprint.pprint(key)
+			if key == "estudiante" or key == 'engresado':
+				pprint.pprint(key)
+				tags_user = User.objects.filter(userext__profile=profile)	
 			ob_users= User.objects.filter(username=key,first_name__contains=key, last_name__contains=key,email__contains=key)
-			
-			for ob_user in ob_users:
+			union_user= ob_users | tags_user
+			pprint.pprint(ob_users)
+			pprint.pprint(tags_user)
+			for ob_user in union_user:
 				vec_user.append(dict([('username',ob_user.username),('first_name',ob_user.first_name),("last_name",ob_user.last_name),("email",ob_user.email)]))
-			vec_total= list(set(vec_user_meta_filter) | set(vec_user))
-			retorno = {'error':0,'data':vec_total}
+			
+			retorno = {'error':0,'data':vec_user}
 		else:
 			retorno = {'error':1}						
 	else:
