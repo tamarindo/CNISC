e73b00670e8ce777e909a7318169e7454d0e77b5
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-04-21 20:01:57 -0500

    Agregando URL para conectar facebook

--- apps/oauthSocial/urls.py
+++ apps/oauthSocial/urls.py
@@ -7,6 +7,9 @@ oauthSocial_urls = patterns('apps.oauthSocial.views',
 	url(r'^autentificar_twitter$','autentificar_usuario_twitter',name="autentificar_twitter"),
 	url(r'^callback_twitter$','callbacktwitter',name="callbacktwitter"),
 	url(r'^configurar_app/(\w+)/$','configura_app',name="configura_app"),
+
+	# Facebook
+	url(r'^facebook_connect/$','facebook_connect',name="facebook_connect"),
 	
 )     
   
