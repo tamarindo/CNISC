726213a6502608343e71c40a9e8a64166093d21f
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-04-27 20:24:49 -0500

    Mejorando la forma en que se eliminan y agregan usuarios a lista de destinatarios, también se valida si el usuario ya fue agregado :)
    Lodash to the rescue!

--- public/static/js/app/admin/admin.createMessage.js
+++ public/static/js/app/admin/admin.createMessage.js
@@ -1,6 +1,8 @@
 /*
  * Aplicacion que provee el controlador para autocompletar la lista de contactos
  * validar y enviar el mensaje
+ * 
+ * @require lodash
  */
 
  (function(){
@@ -28,20 +30,40 @@
 	app.controller('formController', ['$scope', 'ApiTags', function($scope, ApiTags){
 
 		$scope.message = {};
-    $scope.ccList = [];
-    $scope.selectedUsers = [];
     $scope.hideSpinner = true;
+    $scope.ccList = []; // Carga los usuarios recibidos por la API para autocompletar
+    $scope.selectedUsers = []; // Usuarios seleccionados para enviar mensajes
 
+
+    // Determina si se debe esconder la lista de autocompletado
     $scope.hideList = function() {
       return $scope.ccList.length === 0;
     }
 
+
+    // Determina si se debe esconder la lista de usuarios seleccionados
     $scope.hideSelectedUsers = function() {
       return $scope.selectedUsers.length === 0;
     }
 
+
+    // Valida si un usuario ya está incluido en la lista de envíos
+    // @return bool
+    var userExist = function( user ) {
+      var index = _.findIndex($scope.selectedUsers, function(u) {
+        return u.username === user.username;
+      });
+
+      return (index === -1 ? false : true);
+
+    };
+
+
+    // Función de autocompletado
+    // Hace peticiones a la API según una cadena y presenta las coincidencias
+    // Sólo se hacen llamados cuando el texto ingresado es mayor de 4 caracteres
     $scope.autocomplete = function() {
-      var key = $scope.message.to, m;
+      var key = $scope.message.to;
 
       if( key.length < 4 ) {
         $scope.ccList = [];
@@ -55,7 +77,7 @@
 
         var isError = parseInt(response.error, 10);
         if(isError) {
-          console.log('Amm.. algo salió mal con el autompletador. ' + isError);
+          console.log('Amm.. algo salió mal con el autompletador o no se encontró lo que se buscaba. ' + isError);
           return -1;
         }
 
@@ -67,26 +89,42 @@
         // Agregar nuevos mensajes
         if ( list.length > 0 ) {
           for( var i in list ) {
+            if( userExist(list[i]) ) {  // O(n^2) :/
+              list[i].selected = true;
+            }
             $scope.ccList.push( list[i] );
           }
         }
 
       });
-
     }
 
+
+    // Agrega un usuario a las listas
     $scope.add = function(user, e) {
+
+      // Si el usuario seleccionado ya existe, se elimina su selección y de la lista
+      if( userExist(user) ) {
+        user.selected = false;
+        $scope.remove(user, e);
+        return;
+      }
+
       user.selected = true;
       $scope.selectedUsers.push( user );
-      
+
       if(e) {
         e.stopPropagation();
         e.preventDefault();
       }
     }
 
+
+    // Eliminar un usuario de las listas
     $scope.remove = function(user, e) {
-      $scope.selectedUsers.pop( user );
+      _.remove($scope.selectedUsers, function(u) {
+        return u.username === user.username;
+      });
 
       if(e) {
         e.stopPropagation();
@@ -94,10 +132,14 @@
       }
     }
 
+
+    // Hace un reset de las listas, sucede siempre que se da click en cualquier lugar
+    // Diferente al input de Destinatarios.
     $scope.reset = function() {
       $scope.ccList = [];
       $scope.message.to = '';
     }
+
 
     $scope.submit = function() {
       // @TODO 
