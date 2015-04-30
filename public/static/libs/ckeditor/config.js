ef880d7925949a3a19d1a95a31c82b8ad86e2745
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-04-30 16:45:43 -0500

    Personalizando opciones de CKEditor
    * Lenguaje en español
    * Color del layout
    * Tamaño
    * Botones

--- public/static/libs/ckeditor/config.js
+++ public/static/libs/ckeditor/config.js
@@ -5,6 +5,26 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	customConfig : '';
+
+	config.language = 'es';
+	config.uiColor = '#eaeaea';
+	
+	config.height = 280;
+	config.autoGrow_minHeight = 200;
+	config.autoGrow_onStartup = true;
+ 
+	config.toolbar = 'CniscSimple';
+	config.toolbar_CniscSimple =
+	[
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+		{ name: 'insert', items : [ 'Image','Table','HorizontalRule','PageBreak'] },
+                '/',
+		{ name: 'styles', items : [ 'Styles','Format' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+		{ name: 'tools', items : [ 'Maximize' ] }
+	];
 };
