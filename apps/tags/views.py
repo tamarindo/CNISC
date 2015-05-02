16c19c3e9b0bd1335850884abd5e67f00de92d46
Jonathan Alvarez <jonas.al.co@gmail.com>
2015-05-02 13:57:15 -0500

    Enviando en el autocompletado coincidencias con las listas de envío predefinidas. Ver #41
    closes #41

--- apps/tags/views.py
+++ apps/tags/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
@@ -10,20 +11,54 @@ import pprint
 import json
 # Create your views here.
 
+# Crea la lista de envíos predefinida
+def loadPredefinedList() :
+	predefinedList = []
+
+	predefinedList.append(dict([ ('username','todos'), ('firstName','Todos los usuarios'), ('lastName',''), ('email','') ]))
+	predefinedList.append(dict([ ('username','estudiantes'), ('firstName','Todos los estudiantes'), ('lastName',''), ('email','') ]))
+	predefinedList.append(dict([ ('username','egresados'), ('firstName','Todos los egresados'), ('lastName',''), ('email','') ]))
+
+	for i in range(1, 11) :
+		predefinedList.append(dict([ ('username','semestre_' + str(i)), ('firstName','Semestre'), ('lastName', str(i)), ('email','') ]))
+
+	return predefinedList
+
+
+# Filtra una lista de objetos.
+# El objeto se filtra en su atributo firstName según coincidencias con @tag
+#
+# @param list : lista de objetos con un atributo firstName
+# @param tag : cadena de texto a buscar
+#
+# @return: (list)
+def filter_by_tag( list, tag ) :
+	found = []
+	s = tag.lower()
+	for el in list:
+		value = el['firstName'].lower()
+		if value.find(s) != -1 :
+			found.append(el)
+
+	return found
+
 
 def autocomplete(request,*args):
 	key = args[0]
 	min_length_for_query = 4
 	retorno = {'error':1}
 
-	pprint.pprint(key)
+	predefinedList = loadPredefinedList()
 	
 	if key == None or len(key) < min_length_for_query :
 		return HttpResponse(json.dumps(retorno),content_type="application/json")
 
 	if request.method == 'GET' :
 
-		vec_user=[]
+		# Los resultados se inicializan con las coincidencias de la lista
+		# de envio predefinida
+		results = filter_by_tag(predefinedList, key)
+		
 		ob_users= User.objects.none()
 
 		ob_users = User.objects.filter(
@@ -36,14 +71,14 @@ def autocomplete(request,*args):
 
 		# Prepare response
 		for user in ob_users:
-			vec_user.append(dict([
+			results.append(dict([
 				('username',user.username),
 				('firstName',user.first_name),
 				("lastName",user.last_name),
 				("email",user.email)
 			]))
 		
-		retorno = {'error':0,'data':vec_user}
+		retorno = {'error':0,'data':results}
 
 		return HttpResponse(json.dumps(retorno),content_type="application/json")	
 
